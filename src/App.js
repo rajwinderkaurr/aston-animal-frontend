73c583182639e99132db59be335ba673d436f7da
Rajwinder kaur <rajwinderrr@gmail.com>
2021-05-08 16:39:44 +0530

    added functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React from "react";
-import Header from './components/headers/Header'
-import Footer from './components/footers/Footer'
-import DataProvider from './GlobalState'
-import MainPages from './components/mainpages/MainPages'
-import { ToastProvider } from 'react-toast-notifications';
-
-import { BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
   return (
-    <DataProvider className="app">  
-      <ToastProvider>
-        <Router>
-          <Header />
-          <MainPages />
-          <Footer />
-        </Router>
-      </ToastProvider>
-    </DataProvider>
+    <>
+      <div className="center">
+        <div className="form">
+          <h1>Project Moved</h1>
+          <h3>Message for the marking professor only</h3>
+          <p>I was having trouble everytime changing something, and the deploy on netlify was going unsuccessful. So, I moved the project to this <a href="172.105.36.209:3000/">URL</a>. Check it out here. Sorry for the inconvinience</p>
+          <a href="http://bit.ly/aston-animal" className="btn">Go to new Project</a>
+        </div>
+      </div>
+    </>
   );
 }
 
