d20e94ed9e991a4292cbf78b18c6fa214fea70bd
Rajwinder kaur <rajwinderrr@gmail.com>
2021-05-13 21:20:14 +0530

    fixed url

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
           <h1>Project Moved</h1>
           <h3>Message for the marking professor only</h3>
           <p>I was having trouble everytime changing something, and the deploy on netlify was going unsuccessful. So, I moved the project to this <a href="172.105.36.209:3000/">URL</a>. Check it out here. Sorry for the inconvinience</p>
-          <a href="https://bit.ly/3f0EQOY" className="btn">Go to new Project</a>
+          <a href="https://bit.ly/2RU1kcc" className="btn">Go to new Project</a>
         </div>
       </div>
     </>
